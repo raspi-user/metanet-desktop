8f8e199e198f8b60450446acad76152cba925501
Brayden Langley <brayden167@gmail.com>
2025-03-03 15:59:39 -0800

    WIP

--- src/pages/Dashboard/MyIdentity/index.jsx
+++ src/pages/Dashboard/MyIdentity/index.jsx
@@ -4,21 +4,21 @@ import React, { useState, useContext, useEffect } from 'react'
 import { Typography, Button, IconButton } from '@mui/material'
 import { makeStyles } from '@mui/styles'
 import style from './style.js'
-import AddIdCertIcon from '../../../images/addIdCertIcon'
+// import AddIdCertIcon from '../../../images/addIdCertIcon'
 import CheckIcon from '@mui/icons-material/Check'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
-import UIContext from '../../../UIContext.js'
-import { decryptCertificateFields } from 'authrite-utils'
-import AddPopularSigniaCertifiersModal from './AddPopularSigniaCertifiersModal.jsx'
-import EyeCon from '@mui/icons-material/Visibility'
-import CertificatesGrid from './CertificatesGrid.jsx'
+// import UIContext from '../../../'
+// import { decryptCertificateFields } from 'authrite-utils'
+// import AddPopularSigniaCertifiersModal from './AddPopularSigniaCertifiersModal.jsx'
+// import EyeCon from '@mui/icons-material/Visibility'
+// import CertificatesGrid from './CertificatesGrid.jsx'
 
 const useStyles = makeStyles(style, {
   name: 'MyIdentity'
 })
 
 const Trust = ({ history }) => {
-  const { env } = useContext(UIContext)
+  // const { env } = useContext(UIContext)
 
   const [search, setSearch] = useState('')
   const [addPopularSigniaCertifiersModalOpen, setAddPopularSigniaCertifiersModalOpen] = useState(false)
@@ -36,55 +36,55 @@ const Trust = ({ history }) => {
     }, 2000)
   }
 
-  useEffect(() => {
-    const cacheKey = 'provenCertificates'
+  // useEffect(() => {
+  //   const cacheKey = 'provenCertificates'
 
-    const getProvenCertificates = async () => {
-      // Attempt to load the proven certificates from cache
-      const cachedProvenCerts = window.localStorage.getItem(cacheKey)
-      if (cachedProvenCerts) {
-        setCertificates(JSON.parse(cachedProvenCerts))
-      }
+  //   const getProvenCertificates = async () => {
+  //     // Attempt to load the proven certificates from cache
+  //     const cachedProvenCerts = window.localStorage.getItem(cacheKey)
+  //     if (cachedProvenCerts) {
+  //       setCertificates(JSON.parse(cachedProvenCerts))
+  //     }
 
-      // Find and prove certificates if not in cache
-      const certs = await window.CWI.ninja.findCertificates()
-      const provenCerts = []
-      if (certs && certs.certificates && certs.certificates.length > 0) {
-        for (const certificate of certs.certificates) {
-          try {
-            const fieldsToReveal = Object.keys(certificate.fields)
-            const proof = await window.CWI.proveCertificate({
-              certificate,
-              fieldsToReveal,
-              verifierPublicIdentityKey: '0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798'
-            })
-            const decrypted = await decryptCertificateFields(certificate, proof.keyring, '0000000000000000000000000000000000000000000000000000000000000001')
-            proof.decryptedFields = decrypted
-            provenCerts.push(proof)
-          } catch (e) {
-            console.error(e)
-          }
-        }
-        if (provenCerts.length > 0) {
-          setCertificates(provenCerts)
-          window.localStorage.setItem(cacheKey, JSON.stringify(provenCerts))
-        }
-      }
-    }
+  //     // Find and prove certificates if not in cache
+  //     const certs = await window.CWI.ninja.findCertificates()
+  //     const provenCerts = []
+  //     if (certs && certs.certificates && certs.certificates.length > 0) {
+  //       for (const certificate of certs.certificates) {
+  //         try {
+  //           const fieldsToReveal = Object.keys(certificate.fields)
+  //           const proof = await window.CWI.proveCertificate({
+  //             certificate,
+  //             fieldsToReveal,
+  //             verifierPublicIdentityKey: '0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798'
+  //           })
+  //           const decrypted = await decryptCertificateFields(certificate, proof.keyring, '0000000000000000000000000000000000000000000000000000000000000001')
+  //           proof.decryptedFields = decrypted
+  //           provenCerts.push(proof)
+  //         } catch (e) {
+  //           console.error(e)
+  //         }
+  //       }
+  //       if (provenCerts.length > 0) {
+  //         setCertificates(provenCerts)
+  //         window.localStorage.setItem(cacheKey, JSON.stringify(provenCerts))
+  //       }
+  //     }
+  //   }
 
-    getProvenCertificates()
+  //   getProvenCertificates()
 
-    // Set primary identity key
-    const setIdentityKey = async () => {
-      setPrimaryIdentityKey(await window.CWI.getPublicKey({ identityKey: true }))
-    }
+  //   // Set primary identity key
+  //   const setIdentityKey = async () => {
+  //     setPrimaryIdentityKey(await window.CWI.getPublicKey({ identityKey: true }))
+  //   }
 
-    setIdentityKey()
-  }, [setCertificates, setPrimaryIdentityKey])
+  //   setIdentityKey()
+  // }, [setCertificates, setPrimaryIdentityKey])
 
   const handleRevealSecureKey = async () => {
     try {
-      setPrivilegedIdentityKey(await window.CWI.getPublicKey({
+      setPrivilegedIdentityKey(await window.authManager.underlying.getPublicKey({
         identityKey: true,
         privileged: true,
         reason: 'Reveal your secure identity key alongside your everyday one.'
@@ -110,7 +110,7 @@ const Trust = ({ history }) => {
           {copied.id ? <CheckIcon /> : <ContentCopyIcon fontSize='small' />}
         </IconButton>
       </Typography>
-      <Typography variant='body' color='textSecondary'>
+      {/* <Typography variant='body' color='textSecondary'>
         <b>Secure Identity Key:</b> {privilegedIdentityKey === '...' ? <IconButton onClick={handleRevealSecureKey}><EyeCon /></IconButton> : privilegedIdentityKey}
         {privilegedIdentityKey !== '...' && <IconButton size='small' onClick={() => handleCopy(privilegedIdentityKey, 'id')} disabled={copied.id}>
           {copied.id ? <CheckIcon /> : <ContentCopyIcon fontSize='small' />}
@@ -127,7 +127,7 @@ const Trust = ({ history }) => {
           paddingBottom: '1em'
         }}>
         As you go about your life, people and businesses you interact with can give you certificates and credentials for your qualifications. You can also register with popular certifiers so you show up in apps when people interact with you.
-      </Typography>
+      </Typography> */}
       {/* <TextField TODO: Search certificates
         value={search}
         onChange={(e => setSearch(e.target.value))}
@@ -161,7 +161,7 @@ const Trust = ({ history }) => {
           />
         </div>)}
       </div> */}
-      <CertificatesGrid certificates={shownCertificates} />
+      {/* <CertificatesGrid certificates={shownCertificates} />
 
       {shownCertificates.length === 0 && (
         <Typography align='center' color='textSecondary' style={{ marginTop: '2em' }}>No Certificates!</Typography>
@@ -195,7 +195,7 @@ const Trust = ({ history }) => {
             {certificates.length === 0 ? 'Register your identity' : 'Popular Certifiers'}
           </Button>
         </div>
-      </center>
+      </center> */}
     </div>
   )
 }
