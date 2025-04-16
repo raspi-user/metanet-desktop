cc46414fa70a260b6083f1e8b1be4d87cb41ad21
Brayden Langley <brayden167@gmail.com>
2025-04-16 10:00:47 -0700

    Working state - with no tray

--- src/main.tsx
+++ src/main.tsx
@@ -5,28 +5,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import { UserInterface } from '@bsv/brc100-ui-react-components'
 import { onWalletReady } from './onWalletReady'
 import ErrorBoundary from './ErrorBoundary'
-import ThemeWrapper from './ThemeWrapper'
 import { tauriFunctions } from './tauriFunctions'
-
-// Create a simple UI component as fallback
-const SimplePlaceholderUI = () => {
-  return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100%',
-      padding: '2rem',
-      width: '100%',
-      maxWidth: '100%',
-      overflow: 'hidden'
-    }}>
-      <h1>Metanet Desktop</h1>
-      <p>UI component integration in progress...</p>
-    </div>
-  )
-}
 
 // Create the root and render:
 const rootElement = document.getElementById('root')
@@ -36,16 +15,10 @@ if (rootElement) {
   root.render(
     <React.StrictMode>
       <ErrorBoundary>
-        {/* <ThemeWrapper> */}
-        {/* Attempt to use the main UI component */}
-        <UserInterface 
+        <UserInterface
           onWalletReady={onWalletReady}
           tauriFunctions={tauriFunctions}
         />
-
-        {/* Uncomment this and comment out the line above if there are issues */}
-        {/* <SimplePlaceholderUI /> */}
-        {/* </ThemeWrapper> */}
       </ErrorBoundary>
       <ToastContainer
         position="top-right"
